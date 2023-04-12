64fefae31ae97d9bee44d0289b7383694a69e4fb
Stevie Hryciw <codroid@gmail.com>
2023-04-12 15:16:57 -0700

    Update package hash/URL

--- build.zig
+++ build.zig
@@ -4,10 +4,7 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const lua_dep = b.dependency("lua", .{
-        .target = target,
-        .optimize = optimize,
-    });
+    const lua_dep = b.dependency("lua", .{});
     const lib = b.addSharedLibrary(.{
         .name = "corncob",
         .root_source_file = .{ .path = "src/corncob.zig" },
@@ -20,12 +17,13 @@ pub fn build(b: *std.Build) void {
     // The path is relative to the locally cached version of the package.
     const path = b.global_cache_root.join(b.allocator, &.{
         "p",
-        "122055c92fa2b54a9d244645687f4675efcbf95129f90d97ff646d6f878844472888",
+        "122030407cd43ccf4d0fdf2454ff784b85a1a30a9231506a4b9635e3203c1b893e02",
         "lua",
         "include",
     }) catch unreachable;
     lib.addIncludePath(path);
     // Symbols resolved at load time by Lua
     lib.linker_allow_shlib_undefined = true;
-    lib.install();
+    lib.linkLibC();
+    b.installArtifact(lib);
 }
